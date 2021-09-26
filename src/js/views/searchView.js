12070e49a66850bee973afe9b61315b2cc3845b9
okkashaally <steamnature006@gmail.com>
2021-09-26 18:58:53 +0300

    Finished V1 fixed closing preview when searching

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,20 +1,27 @@
 class SearchView {
   _parentElement = document.querySelector(".header__search");
+  _searchInput = document.querySelector(".header__search-input");
+  _preview = document.querySelector(".buy");
 
   getQuery() {
-    const query = this._parentElement.querySelector(
-      ".header__search-input"
-    ).value;
+    const query = this._searchInput.value;
     // this._clearInput();
     return query;
   }
 
   _clearInput() {
-    this._parentElement.querySelector(".header__search-input").value = "";
+    this._searchInput.value = "";
+  }
+
+  _closePreview() {
+    if ((this._preview.style.transform = "translateX(0px)")) {
+      this._preview.style.transform = "translateX(50rem)";
+    }
   }
 
   addHandlerSearch(handler) {
     this._parentElement.addEventListener("submit", (e) => {
+      this._closePreview();
       e.preventDefault();
       handler();
     });
